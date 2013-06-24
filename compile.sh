08d36c020a563181814419c6ee11d696c33debf7
Rud <rudolph.sand@gmail.com>
2013-06-24 03:55:21 +0200

    introduced apc prime, added refDevCnt

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export home=`pwd`
+if [ ! -d $home/node_modules ]; then echo "!! run npm install first !!"; exit 1; fi
+
 for i in `find node_modules | grep gyp | grep -v build`
 do
   cd $home
